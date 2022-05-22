a4a0ce7813bed34fb4b210212c4ebd2ebd41ce04
oluwadaprof <adeekotobiloba8@gmail.com>
2022-05-22 04:46:11 +0100

    goodnight

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,6 +3,7 @@ import './about.css'
 import aboutMe from '../../assets/videocall-designify.png'
 import { FaAward } from "react-icons/fa";
 import { FiUsers } from "react-icons/fi";
+import { VscFolderOpened } from "react-icons/vsc";
 
 
 const About = () => {
@@ -13,7 +14,7 @@ const About = () => {
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
-            <img src={aboutMe} alt="About me" />
+            {/* <img src={aboutMe} alt="About me" /> */}
           </div>
         </div>
         <div className="about__content">
@@ -31,11 +32,15 @@ const About = () => {
             </article>
 
             <article className='about__card' >
-              <FiUsers className='about__icon' />
+              <VscFolderOpened className='about__icon' />
               <h5>Projects</h5>
               <small>6+ completed</small>
             </article>
           </div>
+          <p>Lorem ipsum dolor sit amet consectetur,
+             adipisicing elit. Porro dolore vel expedita blanditiis quo voluptatibus esse repellat culpa recusandae dignissimos necessitatibus perferendis veritatis, 
+            similique et nesciunt nemo, quasi fuga molestiae?</p>
+            <a href="#contact" className='btn btn-primary'>Let's Talk</a>
         </div>
       </div>
     </section>
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -3,7 +3,14 @@ import './experience.css'
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className='container experience__container' >
+
+      </div>
+    </section>
   )
 }
 
