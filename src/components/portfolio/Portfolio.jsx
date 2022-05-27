e8d575f5fc25b1cf67f23edb5a7af092a0c8cc25
oluwadaprof <adeekotobiloba8@gmail.com>
2022-05-27 12:37:17 +0100

    works section added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
-import './portfolio.css'
+import './portfolio.css';
+import { Porftoliodata } from './PortfolioData';
 
 const Portfolio = () => {
+
+  
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+       {Porftoliodata.map(data => <article className="portfolio__item" key={data.id}>
+          <div className="portfolio__item-image">
+            <img src={data.img1} alt="REP" />
+          </div>
+          <h3>{data.title} </h3>
+          <div className="portfolio__item-cta">
+            <a href="www.github.com" className='btn' >Github</a>
+            <a href="www.github.com" className='btn btn-primary'>Live Demo</a>
+          </div>
+        </article>)}
+
+
+
+      </div>
+    </section>
   )
 }
 
---
+++ src/components/portfolio/PortfolioData.js
@@ -0,0 +1,52 @@
+import Img1 from '../../assets/blog01.png';
+
+export const Porftoliodata = [{
+        id: 1,
+        title: 'crypto currency dashboard',
+        img1: ` ${ Img1 }`,
+        github: 'www.github.com',
+        demo: 'www.dribble.com'
+
+    },
+    {
+        id: 2,
+        title: 'crypto currency dashboard',
+        img1: ` ${ Img1 }`,
+        github: 'www.github.com',
+        demo: 'www.dribble.com'
+
+    },
+    {
+        id: 3,
+        title: 'assignment management website',
+        github: 'www.github.com',
+        img1: ` ${ Img1 }`,
+        demo: 'www.dribble.com'
+
+    },
+    {
+        id: 4,
+        title: 'youtube clone',
+        github: 'www.github.com',
+        img1: ` ${ Img1 }`,
+        demo: 'www.dribble.com'
+
+    },
+    {
+        id: 5,
+        title: 'git repo clone',
+        github: 'www.github.com',
+        img1: ` ${ Img1 }`,
+        demo: 'www.dribble.com'
+
+    },
+    {
+        id: 6,
+        title: 'e-commerce website',
+        github: 'www.github.com',
+        img1: ` ${ Img1 }`,
+        demo: 'www.dribble.com'
+
+    }
+
+]
